ff535c35e05b30644b18461b22b989d3071c0f90
laurent.gantel <laurent.gantel@hesge.ch>
2024-10-25 13:34:24 +0200

    custom_apps - Add test application

---
+++ hailort/hailo_test/hailo_test.hpp
@@ -0,0 +1,11 @@
+#ifndef HAILO_TEST_HPP
+#define HAILO_TEST_HPP
+
+#include "hailo/hailort.h"
+#include "hailo/expected.hpp"
+#include "hailo/device.hpp"
+#include <string>
+
+using namespace hailort;
+
+#endif // HAILO_TEST_HPP
